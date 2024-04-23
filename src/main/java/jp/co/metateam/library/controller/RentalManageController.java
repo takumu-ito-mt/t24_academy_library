9bd1ab63992ea5c8f33c518a6788e796c4e2d23a
takumu-ito-mt <takumu.ito@metateam.co.jp>
2024-04-23 18:06:20 +0900

    rental_book

--- src/main/java/jp/co/metateam/library/controller/RentalManageController.java
+++ src/main/java/jp/co/metateam/library/controller/RentalManageController.java
@@ -10,6 +10,20 @@ import jp.co.metateam.library.service.RentalManageService;
 import jp.co.metateam.library.service.StockService;
 import lombok.extern.log4j.Log4j2;
 
+import java.util.List;
+import jp.co.metateam.library.model.RentalManage;
+
+import jp.co.metateam.library.model.RentalManageDto;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import jp.co.metateam.library.model.Account;
+import jp.co.metateam.library.model.Stock;
+import jp.co.metateam.library.values.RentalStatus;
+
+
 /**
  * 貸出管理関連クラスß
  */
@@ -39,12 +53,54 @@ public class RentalManageController {
      */
     @GetMapping("/rental/index")
     public String index(Model model) {
-        // 貸出管理テーブルから全件取得
-
+        // 貸出管理テーブルから全件取得 <>の中はテーブル名
+        List <RentalManage> rentalManageList = this.rentalManageService.findAll();
         // 貸出一覧画面に渡すデータをmodelに追加
-
+        model.addAttribute("rentalManageList", rentalManageList);
         // 貸出一覧画面に遷移
-        return "";
+        return "rental/index";
     }
 
-}
+     @GetMapping("/rental/add")
+      public String add(Model model) {
+        
+          List<Account> accounts = this.accountService.findAll();
+          List<Stock> stockList = this.stockService.findAll();
+        
+
+          model.addAttribute("accounts", accounts);
+          model.addAttribute("stockList", stockList);
+          model.addAttribute("rentalStatus", RentalStatus.values());
+
+        if (!model.containsAttribute("rentalManageDto")) {
+            model.addAttribute("rentalManageDto", new RentalManageDto());
+         }
+          return "rental/add";
+     }
+  
+     @PostMapping("/rental/add")
+        public String save(@Valid @ModelAttribute RentalManageDto rentalManageDto, BindingResult result, RedirectAttributes ra) {
+          try {
+          if (result.hasErrors()) {
+                 throw new Exception("Validation error.");
+             }
+            // 登録処理
+           this.rentalManageService.save(rentalManageDto);
+
+           return "redirect:/rental/index";
+          } catch (Exception e) {
+             log.error(e.getMessage());
+
+              ra.addFlashAttribute("rentalManageDto", rentalManageDto);
+             ra.addFlashAttribute("org.springframework.validation.BindingResult.rentalManageDto", result);
+
+              return "redirect:/rental/add";
+         }
+    } 
+   
+    
+    }
+
+
+
+   
